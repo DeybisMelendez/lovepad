a983fdb64c84a89f0ee237d18e09a220b74bead8
DeybisMelendez <pixelweb2007@gmail.com>
2019-08-13 20:59:03 -0600

    first version v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # lovepad
-virtual gamepad for love2d
+Virtual gamepad for love2d
+
+## How to use
+
+Download `lovepad.lua` and write in your `main.lua`:
+```lua
+lovepad = require "lovepad"
+```
+
+## Creating a button
+
+```lua
+lovepad:new{
+id = "name",
+x = 20,
+y = 50,
+radius = 50,
+-- ...
+}
+```
+Note: The button is instanced in `lovepad.buttons`.
+
+## Button Propierties
+
+| Propierty | Type   |
+|-----------|:------:|
+|     x     | number |
+|     y     | number |
+|   radius  | number |
+|    id     | string |
+|   font    | Font   |
+|colorFont  | table {r, g, b, a} |
+|normalColor| table {r, g, b, a} |
+|pressedColor| table {r, g, b, a} |
+|    mode     | string ("fill" or "line") |
+
+## Functions
+
+```lua
+lovepad:draw()
+```
+Draws the buttons.
+
+```lua
+lovepad:update()
+```
+Updates the buttons.
+
+```lua
+lovepad:isDown(id)
+```
+Returns `true` if the button `id` is pressed.
+```lua
+lovepad:remove(id)
+```
+Removes the button `id`
--- lovepad.lua
+++ lovepad.lua
@@ -1,4 +1,31 @@
-local lovepad = {buttons = {}}
+--[[
+Lovepad
+Developer: Deybis Melendez
+Web: https://github.com/DeybisMelendez/lovepad
+MIT License
+
+Copyright (c) 2019 Deybis Melendez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+--]]
+
+local lovepad = {buttons = {} _VERSION = "v0.1.0", _TYPE= "module", _NAME = "lovepad"}
 local mt = {x = 0, y = 0, radius = 40, id = "button", font = love.graphics.getFont(), colorFont = {1, 1, 1, 1},
 normalColor = {1, 0, 0, 1}, pressedColor = {0,1,0,0.5}, _color = {0, 0, 0, 0}, mode = "fill"}
 function lovepad:new(o)
