af54145b3454ad30b24da36ca1b8993ca1e660e0
DeybisMelendez <pixelweb2007@gmail.com>
2019-08-14 17:55:05 -0600

    v1.0.0 + main.lua para ejemplo

--- lovepad.lua
+++ lovepad.lua
@@ -25,34 +25,40 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 --]]
 
-local lovepad = {buttons = {} _VERSION = "v0.1.0", _TYPE= "module", _NAME = "lovepad"}
-local mt = {x = 0, y = 0, radius = 40, id = "button", font = love.graphics.getFont(), colorFont = {1, 1, 1, 1},
-normalColor = {1, 0, 0, 1}, pressedColor = {0,1,0,0.5}, _color = {0, 0, 0, 0}, mode = "fill"}
+local lovepad = {buttons = {}, _VERSION = "v1.0.0", _TYPE= "module", _NAME = "lovepad"}
+local mt = {x = 0, y = 0, radius = 40, text = "button", font = love.graphics.getFont(), colorFont = {1, 1, 1, 1},
+normalColor = {1, 0, 0, 1}, pressedColor = {0,1,0,0.5}, mode = "fill",
+isDown = false, _lastIsDown = false}
+
 function lovepad:new(o)
 	o = o or {}
 	setmetatable(o, {__index = mt})
-	table.insert(self.buttons, o)
+    self.buttons[o.text] = o
 end
 
 function lovepad:draw()
-	for _, button in ipairs(self.buttons) do
-		love.graphics.setColor(button._color)
-		love.graphics.circle(button.mode, button.x, button.y, button.radius)
-		love.graphics.setColor(button.colorFont)
-		love.graphics.printf(button.id, button.x - button.radius, button.y - button.font:getHeight()/2, button.radius * 2, "center")
+	for index, button in pairs(self.buttons) do
+        if button.isDown then
+            love.graphics.setColor(button.pressedColor)
+        else
+            love.graphics.setColor(button.normalColor)
+        end
+        love.graphics.circle(button.mode, button.x, button.y, button.radius)
+        love.graphics.setColor(button.colorFont)
+		love.graphics.printf(button.text, button.font, button.x - button.radius,
+            button.y - button.font:getHeight()/2, button.radius * 2, "center")
 	end
 	love.graphics.setColor(1, 1, 1, 1)
 end
 
 function lovepad:update()
 	local touches = love.touch.getTouches()
-	for _, button in ipairs(self.buttons) do
-		button._color = button.normalColor
+	for _, button in pairs(self.buttons) do
+        button._lastIsDown = button.isDown
 		button.isDown = false
-		for index, touch in ipairs(touches) do
+		for _, touch in ipairs(touches) do
 		    local xt, yt = love.touch.getPosition(touch)
 			if (math.abs((xt - button.x))^2 + math.abs((yt - button.y))^2)^0.5 < button.radius then
-				button._color = button.pressedColor
 				button.isDown = true
 			end
 		end
@@ -60,17 +66,92 @@ function lovepad:update()
 end
 
 function lovepad:isDown(id)
-	for _, button in ipairs(self.buttons) do
-		if button.id == id then return button.isDown end
-	end
+    return self.buttons[id].isDown
+end
+
+function lovepad:isPressed(id)
+    return self.buttons[id].isDown and not self.buttons[id]._lastIsDown
+end
+
+function lovepad:isReleased(id)
+    return not self.buttons[id].isDown and self.buttons[id]._lastIsDown
 end
 
 function lovepad:remove(id)
-    for index, button in ipairs(self.buttons) do
-        if button.id == id then table.remove(self.buttons, index)
-		return true
-    end
-	return false
+    table.remove(self.buttons, id)
 end
 
+function lovepad:setGamePad(radius, xy, font)
+    local width = love.graphics.getWidth()
+    local height = love.graphics.getHeight()
+    font = font or love.graphics.getFont()
+    radius = radius or width/24
+    xy = xy or false
+    self:new{
+        text = 'Down',
+        radius = radius,
+        x = radius * 3,
+        y = height - radius * 1.25,
+        normalColor = {0.8,0.8,0.8,0.5},
+        pressedColor = {0.4,0.4,0.4,0.5}
+    }
+    self:new{
+        text = 'Up',
+        radius = radius,
+        x = radius * 3,
+        y = height - radius * 4.5,
+        normalColor = {0.8,0.8,0.8,0.5},
+        pressedColor = {0.4,0.4,0.4,0.5}
+    }
+    self:new{
+        text = 'Left',
+        radius = radius,
+        x = radius * 1.25,
+        y = height - radius * 2.75,
+        normalColor = {0.8,0.8,0.8,0.5},
+        pressedColor = {0.4,0.4,0.4,0.5}
+    }
+    self:new{
+        text = 'Right',
+        radius = radius,
+        x = radius * 4.75,
+        y = height - radius * 2.75,
+        normalColor = {0.8,0.8,0.8,0.5},
+        pressedColor = {0.4,0.4,0.4,0.5}
+    }
+    self:new{
+        text = 'A',
+        radius = radius,
+        x = width - radius * 1.25,
+        y = height - radius * 2.75,
+        normalColor = {0.9,0.1,0.1,0.5},
+        pressedColor = {0.4,0.4,0.4,0.5}
+    }
+    self:new{
+        text = 'B',
+        radius = radius,
+        x = width - radius * 3,
+        y = height - radius * 1.25,
+        normalColor = {0,0.9,0,0.5},
+        pressedColor = {0.4,0.4,0.4,0.5}
+    }
+    if xy then
+        self:new{
+            text = 'X',
+            radius = radius,
+            x = width - radius * 3,
+            y = height - radius * 4.5,
+            normalColor = {0.9,0.9,0,0.5},
+            pressedColor = {0.4,0.4,0.4,0.5}
+        }
+        self:new{
+            text = 'Y',
+            radius = radius,
+            x = width - radius * 4.75,
+            y = height - radius * 2.75,
+            normalColor = {0,0,0.9,0.5},
+            pressedColor = {0.4,0.4,0.4,0.5}
+        }
+    end
+end
 return lovepad
---
+++ main.lua
@@ -0,0 +1,36 @@
+lovepad = require 'lovepad'
+lovepad:setGamePad(30, true)
+local text, upr, upp, downr, downp, leftr, leftp, rightr, rightp, ar, ap, br, bp, xr, xp, yr, yp = '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+function love.draw()
+    love.graphics.printf(text, 0, 0, love.graphics.getWidth())
+    lovepad:draw()
+end
+function love.update(dt)
+    lovepad:update()
+    text = 'Button Up --> isDown: ' .. tostring(lovepad:isDown('Up')) .. ' Released count: ' .. upr .. ' Pressed count: ' .. upp ..
+    '\n' .. 'Button Down --> isDown: ' .. tostring(lovepad:isDown('Down')) .. ' Released count: ' .. downr .. ' Pressed count: ' .. downp ..
+    '\n' .. 'Button Left --> isDown: ' .. tostring(lovepad:isDown('Left')) .. ' Released count: ' .. leftr .. ' Pressed count: ' .. leftp ..
+    '\n' .. 'Button Right --> isDown: ' .. tostring(lovepad:isDown('Right')) .. ' Released count: ' .. rightr .. ' Pressed count: ' .. rightp ..
+    '\n' .. 'Button A --> isDown: ' .. tostring(lovepad:isDown('X')) .. ' Released count: ' .. ar .. ' Pressed count: ' .. ap ..
+    '\n' .. 'Button B --> isDown: ' .. tostring(lovepad:isDown('B')) .. ' Released count: ' .. br .. ' Pressed count: ' .. bp ..
+    '\n' .. 'Button X --> isDown: ' .. tostring(lovepad:isDown('X')) .. ' Released count: ' .. xr .. ' Pressed count: ' .. xp ..
+    '\n' .. 'Button Y --> isDown: ' .. tostring(lovepad:isDown('Y')) .. ' Released count: ' .. yr .. ' Pressed count: ' .. yp
+
+    if lovepad:isPressed('Up') then upp = upp + 1 end
+    if lovepad:isPressed('Down') then downp = downp + 1 end
+    if lovepad:isPressed('Left') then leftp = leftp + 1 end
+    if lovepad:isPressed('Right') then rightp = rightp + 1 end
+    if lovepad:isPressed('A') then ap = ap + 1 end
+    if lovepad:isPressed('B') then bp = bp + 1 end
+    if lovepad:isPressed('X') then xp = xp + 1 end
+    if lovepad:isPressed('Y') then yp = yp + 1 end
+
+    if lovepad:isReleased('Up') then upr = upr + 1 end
+    if lovepad:isReleased('Down') then downr = downr + 1 end
+    if lovepad:isReleased('Left') then leftr = leftr + 1 end
+    if lovepad:isReleased('Right') then rightr = rightr + 1 end
+    if lovepad:isReleased('A') then ar = ar + 1 end
+    if lovepad:isReleased('B') then br = br + 1 end
+    if lovepad:isReleased('X') then xr = xr + 1 end
+    if lovepad:isReleased('Y') then yr = yr + 1 end
+end
